4dd5ad28d2630827ffba344a8a245a765ccfc0eb
Pandu <pandurang.yachwad@gmail.com>
2017-02-22 16:23:33 -0500

    FUTURE comment

--- Music Search/ViewController.swift
+++ Music Search/ViewController.swift
@@ -9,7 +9,8 @@
 
 /*
     This project loosely follows MVC model, given more time, effective MVC can be followed for better results. Also not all corner cases are not considered, e.g. handling of unknown errors, incorrect string search, etc. 
-     Attention is given to functionality and ability to search using public API's. Used JSON parsing and webcontent parsing for reading the data
+    Attention is given to functionality and ability to search using public API's. Used JSON parsing and webcontent parsing for reading the data
+    Also, there is scope for layout support for landscape
  */
 
 import UIKit
